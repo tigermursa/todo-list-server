df6a74db39572ca67f55b3fed66ce0cc8a351868
mursalin@gmail.com <MURSA>
2023-06-05 23:23:30 +0600

    added the updade api by id

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 // MONGODB CODE >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1nqrclq.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -45,14 +45,14 @@ async function run() {
         res.send(result);
       });
       //2.  GET /READ FROM HERE......
-      app.get("/users", async (req, res) => {
+      app.get("/task", async (req, res) => {
         const cursor = theCollection.find();
         const result = await cursor.toArray();
         res.send(result);
       });
   
       //3. UPDATE FROM HERE .....................................................,,
-      app.get("/users/:id", async (req, res) => {
+      app.get("/task/:id", async (req, res) => {
         const id = req.params.id;
         console.log("updating ", id);
         const query = { _id: new ObjectId(id) };
